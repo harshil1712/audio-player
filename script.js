d2af14697352cadc09636055b86008be90b2700f
ghagrawal17@gmail.com <ghagrawal17@gmail.com>
2017-05-21 00:39:32 +0530

    Autoplay and audio obj

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-var searchTxt,min=0,sec=0, availableTags = [],cover=[],songURL=[],index=0;
+var searchTxt,min=0,sec=0, availableTags = [],cover=[],songURL=[],index=0,durationTime;
     $.getJSON('http://starlord.hackerearth.com/sureify/cokestudio').done(function(data){
     	for (var i = 0; i < data.length; i++) {    		
     		var title= data[i]['song'],
@@ -9,7 +9,7 @@ var searchTxt,min=0,sec=0, availableTags = [],cover=[],songURL=[],index=0;
             cover[i]=img;
             songURL[i]=url;            
             $('.cover-img').attr('src',cover[0]);
-            $('#song').attr('src',songURL[0]);
+            audio.src = songURL[0];
             $('#dwnld-btn').attr('href',songURL[0]);
     		for (var j =  0; j<=i; j++) {    				
     				if (j==i) {
@@ -26,91 +26,100 @@ var searchTxt,min=0,sec=0, availableTags = [],cover=[],songURL=[],index=0;
             });
     	}
     });
-    var audio = new Audio;
+    var audio = new Audio(songURL);
     $('#slider').slider();
-    var song=$('#song')[0];
+    //var song=$('#song')[0];
     // Play Pause function
     $('#play-btn').click(function(){
         var $this = $(this);
         if($this.hasClass('glyphicon-play')){
             $this.toggleClass('glyphicon-play glyphicon-pause');
-            $('#song').trigger('play');
+            audio.play();
         }
         else{
             $this.toggleClass('glyphicon-pause glyphicon-play');
-            $('#song').trigger('pause'); 
+            audio.pause(); 
         }
     });
     // Progress bar update and timer update
-    song.addEventListener('timeupdate',function (){
-        value = Math.floor((100 / song.duration) * song.currentTime);
+    audio.addEventListener('timeupdate',function (){
+        value = Math.floor((100 / audio.duration) * audio.currentTime);
         $('#head').animate({'margin-left': value+'%'},50,'linear');
-        min=Math.floor((song.duration-song.currentTime)/60,10);
-        sec=Math.floor((((song.duration-song.currentTime)/60)-min)*60,10);
+        min=Math.floor((audio.duration-audio.currentTime)/60,10);
+        sec=Math.floor((((audio.duration-audio.currentTime)/60)-min)*60,10);
         sec= sec>9?sec:'0'+sec;
         //console.log(min,sec);
         $('#timer').text('-'+min+':'+sec);
         $('#slider').slider({
-      value: song.currentTime,
+      value: audio.currentTime,
       orientation: "horizontal",
       range: "min",
-      max: song.duration,
+      max: audio.duration,
       animate: true,
       slide: function() {
         manualSeek = true;
       },
       stop:function(e,ui) {
         manualSeek = false;
-        song.currentTime = ui.value;
+        audio.currentTime = ui.value;
       }
         });
         if(sec==0){
-            min = Math.floor((song.duration/60),10);
-            sec= Math.floor(((song.duration/60)-min)*60,10);
+            min = Math.floor((audio.duration/60),10);
+            sec= Math.floor(((audio.duration/60)-min)*60,10);
             $('#play-btn').removeClass('glyphicon-pause').addClass('glyphicon-play');
             $('#timer').text(min+":"+sec);
             $('#slider').slider({value:0});
         }
+        if(audio.currentTime==audio.duration){
+        index++;
+        audio.src = songURL[index];
+        audio.play();
+        $('#play-btn').toggleClass('glyphicon-play glyphicon-pause');
+        }
     });
     // Getting song duration for timer
-    song.onloadedmetadata = function(){
-        min=Math.floor(song.duration/60,10);
-        sec=Math.floor(((song.duration/60)-min)*60,10);
-        if(song.duration==NaN){
+    audio.onloadedmetadata = function(){
+        min=Math.floor(audio.duration/60,10);
+        sec=Math.floor(((audio.duration/60)-min)*60,10);
+        if(audio.duration==NaN){
             $('#timer').text('0'+':'+'00');
         }
         else{
             sec= sec>9?sec:'0'+sec;
             $('#timer').text(min+':'+sec);
         }
+        durationTime=audio.duration;
+    console.log(durationTime);
+    
     }
     $('#volume').click(function(){
         $(this).toggleClass('glyphicon-volume-up glyphicon-volume-off');
         if($(this).hasClass('glyphicon-volume-off')){
-            song.volume = 0;
+            audio.volume = 0;
         }
         else{
-            song.volume = 1;
+            audio.volume = 1;
         }
     });
     $('#frwd').click(function(){  
         index++;
         $('.cover-img').attr('src',cover[index]);
-        $('#song').attr('src',songURL[index]);
+        audio.src = songURL[index];
         $('#dwnld-btn').attr('href',songURL[index]);
         if($('#play-btn').hasClass('glyphicon-play')){
-            $('#song').trigger('pause');
+            audio.pause();
         }
         else{
             $('#play-btn').toggleClass('glyphicon-pause glyphicon-play');
         }
         if(index>=10){
             if($('#play-btn').hasClass('glyphicon-play')){
-                $('#song').trigger('pause');
+                audio.play();
             }
             else{
                 $('#play-btn').toggleClass('glyphicon-pause glyphicon-play');
-                $('#song').trigger('pause');
+                audio.pause();
             }
             index=9;
         }
@@ -119,14 +128,15 @@ var searchTxt,min=0,sec=0, availableTags = [],cover=[],songURL=[],index=0;
         if(index!=0){
             index--;
             $('.cover-img').attr('src',cover[index]);
-            $('#song').attr('src',songURL[index]);
+            audio.src = songURL[index];
             $('#dwnld-btn').attr('href',songURL[index]);
             if($('#play-btn').hasClass('glyphicon-play')){
-                $('#song').trigger('pause');
+               audio.pause();
             }
             else{
                 $('#play-btn').toggleClass('glyphicon-pause glyphicon-play');
             }
         }
     });
+    
 });
